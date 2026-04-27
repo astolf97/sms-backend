f71205ceda5bd1c6f98ec036a86f3957a66f0752
astolf97 <astolfastolf33@gmail.com>
2026-04-27 14:38:02 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let smsList = [];
+
+app.get("/", (req, res) => {
+    res.send("Backend SMS attivo");
+});
+
+app.post("/sms", (req, res) => {
+    const sms = req.body;
+    smsList.push(sms);
+    console.log("SMS ricevuto:", sms);
+    res.send({ status: "ok" });
+});
+
+app.get("/sms", (req, res) => {
+    res.json(smsList);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server avviato su porta", PORT);
+});
