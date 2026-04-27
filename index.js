e7fa1a11962e65983eca87dba6b4989a5476a127
astolf97 <astolfastolf33@gmail.com>
2026-04-27 18:07:45 +0200

    fix static dashboard

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const path = require("path");
-app.use(express.static(path.join(__dirname)));
-
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+// ✅ serve index.html
+app.use(express.static(path.join(__dirname)));
+
 let smsList = [];
 
+// 👉 opzionale: apre dashboard direttamente su /
 app.get("/", (req, res) => {
-    res.send("Backend SMS attivo");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.post("/sms", (req, res) => {
